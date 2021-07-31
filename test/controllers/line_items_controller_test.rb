ea80f9d7ac4e69c0ad1f52525859f550b9c29c6c
kenny-kogi <mwangiken497@gmail.com>
2021-07-31 13:06:13 +0300

    Creating the cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -15,14 +15,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
-    end
-
-    assert_redirected_to line_item_url(LineItem.last)
-  end
-
   test "should show line_item" do
     get line_item_url(@line_item)
     assert_response :success
@@ -44,5 +36,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to line_items_url
+  end
+  test "should create line_item" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id }
+    end
+
+    follow_redirect!
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', 'Programming Ruby 1.9'
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,4 +46,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "cant delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+  
 end
