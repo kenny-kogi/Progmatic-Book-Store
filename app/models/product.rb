896917a0ef8fa88459bf4c9812b35a8091103c64
kenny-kogi <mwangiken497@gmail.com>
2021-08-03 14:31:16 +0300

    order page

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,14 @@ class Product < ApplicationRecord
         message: 'must be a URL for GIF, JPG or PNG image.'
         }
     has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+    has_many :orders, through: :line_items
 
     private
         def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort               
+            end
         end
 end
